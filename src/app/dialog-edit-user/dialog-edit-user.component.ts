e3262a222ae2f7fe5929be569c3a2ba590a409cd
RaphaelKono <raphaelkonopatzki@web.de>
2023-01-03 15:04:26 +0100

    changed edit user

--- src/app/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -17,21 +17,22 @@ export class DialogEditUserComponent {
   userInProgress = false;
   user: User;
 
-  userForm = new FormGroup({
-    title: new FormControl(this.data.user.title),
-    firstName: new FormControl(this.data.user.firstName, [Validators.required, Validators.minLength(2)]),
-    lastName: new FormControl(this.data.user.lastName, [Validators.required, Validators.minLength(2)]),
-    birthDate: new FormControl(new Date(this.data.user.birthDate.seconds), [Validators.required]),
-    street: new FormControl(this.data.user.street, Validators.required),
-    streetNo: new FormControl(this.data.user.streetNo, [Validators.required, Validators.pattern(this.streetNoPattern)]),
-    postcode: new FormControl(this.data.user.postcode, Validators.required),
-    city: new FormControl(this.data.user.city, Validators.required),
-    telephone: new FormControl(this.data.user.telephone, [Validators.required, Validators.pattern(this.phoneNoPattern)]),
-    email: new FormControl(this.data.user.email, [Validators.required, Validators.email, Validators.pattern(this.emailPattern)])
-  });
+  userForm: FormGroup;
 
   constructor(private firestore: Firestore, public dialogRef: MatDialogRef<DialogEditUserComponent>, @Inject(MAT_DIALOG_DATA) public data: any){
     this.user = data.user;
+    this.userForm = new FormGroup({
+      title: new FormControl(this.user.title),
+      firstName: new FormControl(this.user.firstName, [Validators.required, Validators.minLength(2)]),
+      lastName: new FormControl(this.user.lastName, [Validators.required, Validators.minLength(2)]),
+      birthDate: new FormControl(new Date(this.user.birthDate.seconds), [Validators.required]),
+      street: new FormControl(this.user.street, Validators.required),
+      streetNo: new FormControl(this.user.streetNo, [Validators.required, Validators.pattern(this.streetNoPattern)]),
+      postcode: new FormControl(this.user.postcode, Validators.required),
+      city: new FormControl(this.user.city, Validators.required),
+      telephone: new FormControl(this.user.telephone, [Validators.required, Validators.pattern(this.phoneNoPattern)]),
+      email: new FormControl(this.user.email, [Validators.required, Validators.email, Validators.pattern(this.emailPattern)])
+    });
   }
 
   async onSubmit() {
@@ -47,20 +48,20 @@ export class DialogEditUserComponent {
   }
 
   editUser() {
-    this.data.user.title = this.userForm.value.title;
-    this.data.user.firstName = this.userForm.value.firstName;
-    this.data.user.lastName = this.userForm.value.lastName;
-    this.data.user.birthDate = this.userForm.value.birthDate;
-    this.data.user.street = this.userForm.value.street;
-    this.data.user.streetNo = this.userForm.value.streetNo;
-    this.data.user.postcode = this.userForm.value.postcode;
-    this.data.user.city = this.userForm.value.city;
-    this.data.user.telephone = this.userForm.value.telephone;
-    this.data.user.email = this.userForm.value.email;
+    this.user.title = this.userForm.value.title;
+    this.user.firstName = this.userForm.value.firstName;
+    this.user.lastName = this.userForm.value.lastName;
+    this.user.birthDate = this.userForm.value.birthDate;
+    this.user.street = this.userForm.value.street;
+    this.user.streetNo = this.userForm.value.streetNo;
+    this.user.postcode = this.userForm.value.postcode;
+    this.user.city = this.userForm.value.city;
+    this.user.telephone = this.userForm.value.telephone;
+    this.user.email = this.userForm.value.email;
   }
 
   async saveEditedUser() {
     let coll = collection(this.firestore, 'users');
-    await setDoc(doc(coll, this.data.userID), this.data.user.toJson());
+    await setDoc(doc(coll, this.data.userID), this.user.toJson());
   }
 }
